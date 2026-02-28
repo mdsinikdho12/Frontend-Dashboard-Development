3b3f34c901020e7c4939da9e91fbb5ffaa12f7b1
md sinikdho <mdsinikdho94@gmail.com>
2026-02-28 19:38:38 +0600

    Dashbord protecded

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Route, Router, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import PriveteRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PriveteRoute>
+            <Dashboard />
+          </PriveteRoute>
+        }
+      />
     </Routes>
   );
 }
